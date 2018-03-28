c82e077e79aebf51bc13c86c756ce2ef7f9d8df0
yann <yanndumas54230@gmail.com>
2018-03-28 18:34:37 +0200

    initial commit

---
+++ src/components/MenuPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-ons-page>
+    <v-ons-toolbar modifier="transparent"></v-ons-toolbar>
+
+    <v-ons-list-title>Onsen UI Essential Links</v-ons-list-title>
+  </v-ons-page>
+</template>
+
+<script>
+export default {
+  name: 'menu',
+  data () {
+    return {
+      essentialLinks: []
+    }
+  },
+  methods: {
+    goTo (url) {
+      window.open(url, '_blank')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+img {
+  max-width: 150px;
+}
+
+ons-list-title {
+  text-transform: none;
+}
+
+ons-list-item {
+  cursor: pointer;
+}
+</style>
