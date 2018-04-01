f7b6ee9f451a28cc07dcacd8d475289e62a516ba
dev-yann <yanndumas54230@gmail.com>
2018-04-01 20:36:21 +0200

    test

--- src/components/Geo.vue
+++ src/components/Geo.vue
@@ -29,7 +29,25 @@
 
             document.addEventListener("deviceready", onDeviceReady, false);
             function onDeviceReady() {
-                alert("navigator.geolocation works well");
+
+                var onSuccess = function(position) {
+                    alert('Latitude: '          + position.coords.latitude          + '\n' +
+                        'Longitude: '         + position.coords.longitude         + '\n' +
+                        'Altitude: '          + position.coords.altitude          + '\n' +
+                        'Accuracy: '          + position.coords.accuracy          + '\n' +
+                        'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+                        'Heading: '           + position.coords.heading           + '\n' +
+                        'Speed: '             + position.coords.speed             + '\n' +
+                        'Timestamp: '         + position.timestamp                + '\n');
+                };
+
+                function onError(error) {
+                    alert('code: '    + error.code    + '\n' +
+                        'message: ' + error.message + '\n');
+                }
+
+                navigator.geolocation.getCurrentPosition(onSuccess, onError);
+                //alert("navigator.geolocation works well");
             }
 
         }
